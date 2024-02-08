4a9ed0e5b950e4017e19dc948aca26068cee0afa
Bruno Alexandre Rosa <1791393+brunoalr@users.noreply.github.com>
2024-02-08 09:00:07 -0300

    Use real constraint

--- main.cpp
+++ main.cpp
@@ -344,10 +344,10 @@ using namespace symbolic;
 
 int main()
 {
-    constexpr symbol a;
-    constexpr symbol w;
-    constexpr symbol t;
-    constexpr symbol phi;
+    constexpr symbol<real> a;
+    constexpr symbol<real> w;
+    constexpr symbol<real> t;
+    constexpr symbol<real> phi;
 
     constexpr formula f = a * sin(w * t + phi);
     const auto y = f(a = 5.0, w = 2.5, t = 1.6, phi = 0.0);
