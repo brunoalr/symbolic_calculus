cefa062978ba6649b7b507f619682cfd755ac6f1
Bruno Alexandre Rosa <1791393+brunoalr@users.noreply.github.com>
2024-02-08 15:33:12 -0300

    fixup! fixup! fixup! fixup! Refactor into smaller headers

--- src/utils.h
+++ src/utils.h
@@ -2,6 +2,7 @@
 #define UTILS_H
 
 #include <cmath>
+#include <functional> // for std::plus etc
 #include <type_traits> // for std::type_identity, std::is_lvalue_reference_v, std::remove_reference_t, std::is_rvalue_reference_v<T>, std::conditional etc
 #include <utility> // for std::forward, std::index_sequence_for
 
