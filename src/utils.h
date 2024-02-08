7a9af01661f3bc47f8457bdc6f0d128f7e22e5be
Bruno Alexandre Rosa <1791393+brunoalr@users.noreply.github.com>
2024-02-08 15:48:25 -0300

    fixup! fixup! fixup! fixup! fixup! fixup! Refactor into smaller headers

--- src/utils.h
+++ src/utils.h
@@ -8,59 +8,6 @@
 
 namespace symbolic
 {
-    template <class T>
-    struct remove_lvalue_reference : std::type_identity<T>
-    {
-    };
-
-    template <class T>
-        requires std::is_lvalue_reference_v<T>
-    struct remove_lvalue_reference<T> : std::type_identity<std::remove_reference_t<T>>
-    {
-    };
-
-    template <class T>
-    using remove_lvalue_reference_t = typename remove_lvalue_reference<T>::type;
-
-    template <class T>
-    struct remove_rvalue_reference : std::type_identity<T>
-    {
-    };
-
-    template <class T>
-        requires std::is_rvalue_reference_v<T>
-    struct remove_rvalue_reference<T> : std::type_identity<std::remove_reference_t<T>>
-    {
-    };
-
-    template <class T>
-    using remove_rvalue_reference_t = typename remove_rvalue_reference<T>::type;
-
-    template <class T>
-    struct requalify_as_const
-        : std::conditional<std::is_lvalue_reference_v<T>,
-                           std::add_lvalue_reference_t<std::add_const_t<std::remove_reference_t<T>>>,
-                           std::conditional_t<std::is_rvalue_reference_v<T>,
-                                              std::add_rvalue_reference_t<std::add_const_t<std::remove_reference_t<T>>>,
-                                              std::add_const_t<T>>>
-    {
-    };
-
-    template <class T>
-    using requalify_as_const_t = typename requalify_as_const<T>::type;
-
-    template <class T>
-    struct requalify_as_volatile;
-
-    template <class T>
-    using requalify_as_volatile_t = typename requalify_as_volatile<T>::type;
-
-    template <class T>
-    struct requalify_as_cv;
-
-    template <class T>
-    using requalify_as_cv_t = typename requalify_as_cv<T>::type;
-
     struct unconstrained
     {
         template <class T>
